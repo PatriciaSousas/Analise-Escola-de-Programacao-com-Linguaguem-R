0e401b9eb56c8def9e554c7c4e6f0f8b08d3c18a
Patricia Sousa <81525412+PatriciaSousas@users.noreply.github.com>
2022-07-21 19:14:21 -0300

    Add files via upload

---
+++ Análise estatística.R
@@ -0,0 +1,18 @@
+mean(amostra_de_duracao$dias)
+
+mean(amostra_de_duracao$dias, na.rm= T)
+
+median(amostra_de_duracao$dias, na.rm= T)
+
+summary(amostra_de_duracao$dias)
+
+dim(amostra_de_duracao)[1]
+
+
+3828/6366
+
+
+length(unique(amostra_de_duracao$curso))
+length(unique(amostra_de_duracao$aluno))
+
+
